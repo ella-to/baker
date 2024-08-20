de5eb37cb71db58af9b868d9103f587b8e2bc303
Ali Najafizadeh <a.najafizadeh@gmail.com>
2024-08-20 13:16:21 -0400

    Upgrade to go 1.23 and add prometheus metrics

--- cmd/baker/main.go
+++ cmd/baker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -13,6 +14,7 @@ import (
 	"ella.to/baker/driver"
 	"ella.to/baker/internal/acme"
 	"ella.to/baker/internal/httpclient"
+	"ella.to/baker/internal/metrics"
 	"ella.to/baker/rule"
 )
 
@@ -21,11 +23,13 @@ var GitCommit = "development"
 
 func main() {
 	fmt.Fprintf(os.Stdout, `
-_____       __    
-| __ )  __ _| | _____ _ __ 
-|  _ \ / _  | |/ / _ \ '__|
-| |_) | (_| |   <  __/ |   
-|____/ \__,_|_|\_\___|_|
+
+██████╗░░█████╗░██╗░░██╗███████╗██████╗░
+██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗
+██████╦╝███████║█████═╝░█████╗░░██████╔╝
+██╔══██╗██╔══██║██╔═██╗░██╔══╝░░██╔══██╗
+██████╦╝██║░░██║██║░╚██╗███████╗██║░░██║
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝
                            
 Version: %s
 Git Hash: %s 
@@ -37,8 +41,14 @@ https://ella.to/baker
 	logLevel := strings.ToLower(os.Getenv("BAKER_LOG_LEVEL"))
 	bufferSize := parseInt(os.Getenv("BAKER_BUFFER_SIZE"), 100)
 	pingDuration := parseDuration(os.Getenv("BAKER_PING_DURATION"), 2*time.Second)
+	metricsAddr := os.Getenv("BAKER_METRICS_ADDR")
+	if metricsAddr == "" {
+		metricsAddr = "0.0.0.0:8089"
+	}
 
 	slog.SetLogLoggerLevel(parseLogLevel(logLevel))
+
+	metricsHandler := metrics.SetupHandler()
 
 	dockerGetter, err := httpclient.NewClient(
 		httpclient.WithUnixSock("/var/run/docker.sock", "http://localhost"),
@@ -61,13 +71,30 @@ https://ella.to/baker
 	)
 	handler.RegisterDriver(docker.RegisterDriver)
 
+	metricsServer := http.Server{
+		Addr:    metricsAddr,
+		Handler: metricsHandler,
+	}
+
+	defer metricsServer.Shutdown(context.Background())
+
+	go func() {
+		slog.Info("starting metrics server", "addr", metricsAddr)
+		err := metricsServer.ListenAndServe()
+		if err != nil {
+			slog.Error("failed to start metrics server", "error", err)
+		}
+	}()
+
 	if acmeEnable {
+		slog.Info("starting acme server", "addr", acmePath)
 		err := acme.Start(handler, acmePath)
 		if err != nil {
 			slog.Error("failed to start acme", "error", err)
 			os.Exit(1)
 		}
 	} else {
+		slog.Info("starting server", "addr", ":80")
 		err := http.ListenAndServe(":80", handler)
 		if err != nil {
 			slog.Error("failed to start server", "error", err)
